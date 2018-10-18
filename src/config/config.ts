c1bc925d8660e385039aa10afc428924dd647341
Mahjoub ELATTAR <mahj.attar@yahoo.fr>
2018-10-18 22:34:46 +0200

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,45 @@
+//**************************************************************************************************************
+//If you are going to make changes in this file, remember to make the changes into the deploinment file too
+
+//**************************************************************************************************************
+
+export const config = {
+  apiKey: "AIzaSyBW6PXa8BgsBba-oei6SkYCGWPkZPLmsN0",
+  authDomain: "porscheclubmonacoproject.firebaseapp.com",
+  databaseURL: "https://porscheclubmonacoproject.firebaseio.com",
+  projectId: "porscheclubmonacoproject",
+  storageBucket: "porscheclubmonacoproject.appspot.com",
+  messagingSenderId: "832421749861"
+};
+
+export const payPalEnvironmentSandbox = "<your key here>";
+
+export const webClientId = "832421749861-hggcv7vf5fiq9012o3n7mh5ra23j7dfc.apps.googleusercontent.com";
+
+export const admobAndroid = {
+  banner: "<your key here>",
+  interstitial: "<your key here>",
+  videos: "<your key here>"
+};
+
+export const admobIOS = {
+  banner: "<your key here>",
+  interstitial: "<your key here>",
+  videos: ""
+};
+
+//these are the images for the slides on the login pwa
+export const imagesSlide = [
+  { src: "./assets/slide1.png" },
+  { src: "./assets/slide3.png" },
+  { src: "./assets/slide4.png" },
+  { src: "./assets/slide5.png" }
+];
+
+export const stripeKey = "<your key here>"; //use test key from your account on stripe.com published key
+
+export const stripeBackendEndPoint = "http://192.168.1.12:3333/processpay";
+
+//export const googleMapsApi = "AIzaSyCtKdZQw5WLOLjybIHIw339WV1uv8l83-8";
+//AIzaSyANKDtJgO99dlyxSbtCH30okTkthOxgGVk
+export const googleMapsApi = "AIzaSyCtKdZQw5WLOLjybIHIw339WV1uv8l83-8";
